a7dd324b3115a2679512e7efa3e3d2979b8aa363
Mercy <owinomercy983@gmail.com>
2025-03-10 17:48:41 +0300

    string lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser="mercy"
+let welcomeMessage="Welcome to Flatbook, mercy  !"
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, MERCY !"
+let shortGreeting="Welcome, M !"
